01f6f436b3c77cbdfd2cc9322b3f9872de185040
Bozi Daniel <daniel.bozi88@gmail.com>
2019-12-19 09:55:31 +0100

    making minor changes in item

--- src/main/java/com/codecool/quest/logic/item/Item.java
+++ src/main/java/com/codecool/quest/logic/item/Item.java
@@ -4,13 +4,15 @@ import com.codecool.quest.logic.Cell;
 import com.codecool.quest.logic.Drawable;
 
 public abstract class Item implements Drawable {
-
+    private String name;
     private Cell cell;
-
     public Item(Cell cell) {
         this.cell = cell;
     }
 
-
+    public String giveName(String itemName) {
+        name = itemName;
+        return name;
+    }
 
 }
--- src/main/java/com/codecool/quest/logic/item/Sword.java
+++ src/main/java/com/codecool/quest/logic/item/Sword.java
@@ -6,17 +6,20 @@ import com.codecool.quest.logic.Cell;
 public class Sword extends Item {
     private int damage = 2;
 
-    public Sword(Cell cell){
+    public Sword(Cell cell) {
         super(cell);
     }
 
-
-
-    public int getDamageBonus(){
+    public int getDamageBonus() {
         return this.damage;
     }
 
 
+    private String itemName = giveName("Sword");
+
+    public String getName() {
+        return itemName;
+    }
 
     @Override
     public String getTileName() {
